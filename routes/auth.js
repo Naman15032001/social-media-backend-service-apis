51bfe64cad7a9e1bc02ce6c6e7188158ac92151e
Naman Jain <namanjain15032001@gmail.com>
2022-11-08 18:15:42 +0530

    login

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 import express from 'express';
-import  connection from '../db.js'
+import connection from '../db.js'
 import bcrypt from 'bcrypt';
+import jsonwebtoken from 'jsonwebtoken'
+import secret from '../secret.js';
 
 const authRouter = express.Router();
 
@@ -14,7 +16,7 @@ authRouter.post("/signup", async (req, res) => {
     } = req.body;
 
     password = await bcrypt.hash(password, 10)
-    connection.query("INSERT into users (fullname,password,email,profil_pic) values (?,?,?,?)" ,[fullname, password, email, profil_pic],
+    connection.query("INSERT into users (fullname,password,email,profil_pic) values (?,?,?,?)", [fullname, password, email, profil_pic],
         (err, result) => {
 
             if (err) {
@@ -30,6 +32,58 @@ authRouter.post("/signup", async (req, res) => {
                 })
             }
         })
+
+})
+
+authRouter.post("/login", (req, res) => {
+
+    let {
+        email,
+        password,
+    } = req.body;
+    console.log(req.body);
+    console.log(email)
+    connection.query('select * from users where email=?', [email],
+        async (err, result) => {
+            if (err) {
+                res.json({
+                    error: err.message,
+                    success: 0
+                })
+            } else {
+                if (result.length > 0) {
+
+                    const ans = await bcrypt.compare(password, result[0].password)
+
+                    if (ans) {
+                        delete result[0].password;
+                        const token = await jsonwebtoken.sign({
+                            ...result[0]
+                        }, secret, {
+                            expiresIn: '10h'
+                        })
+                        res.json({
+                            ...result[0],
+                            token
+                        })
+
+                    } else {
+                        res.json({
+                            error: 'incorrect password',
+                            success: 0
+                        });
+
+                    }
+
+                } else {
+                    res.json({
+                        error: "user not found",
+                        success: 0
+                    })
+                }
+            }
+        }
+    )
 
 })
 
