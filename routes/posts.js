0d0eb560e3038ae3bb77efa43cf2f630d86cb8c5
Naman Jain <namanjain15032001@gmail.com>
2022-11-11 20:03:57 +0530

    likes fn

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import connection from '../db.js';
 import authMiddleware from '../middleware/authMiddleware.js'
+import jsonwebtoken from 'jsonwebtoken'
+import secret from '../secret.js';
 
 const postRouter = express.Router();
 
@@ -86,6 +88,144 @@ postRouter.get("/mypost/:id", authMiddleware, (req, res) => {
                 res.json({
                     success: 0,
                     error: "user not found or invalid user id"
+                })
+            }
+        }
+    })
+
+})
+
+postRouter.post("/dolike/:id", authMiddleware, (req, res) => {
+
+    const id = req.params.id;
+
+    const authHeader = req.headers['authorization'];
+
+    console.log("hello1", authHeader)
+
+    const token = authHeader && authHeader.split(" ")[1];
+
+    jsonwebtoken.verify(token, secret, (err, user) => {
+
+        if (err) {
+            res.json({
+                error: "auth failed",
+                success: 0
+            })
+        } else {
+            connection.query("insert into likes (user_id,post_id) values(?,?)", [user.id, id], (err, result) => {
+                if (err) {
+                    res.json({
+                        success: 0,
+                        error: err.message
+                    })
+                } else {
+                    res.json({
+                        success: 1,
+                    })
+                }
+            })
+        }
+    });
+})
+
+postRouter.delete('/dontlike/:id', authMiddleware, (req, res) => {
+
+    const id = req.params.id;
+
+    const authHeader = req.headers['authorization'];
+
+    console.log("hello1", authHeader)
+
+    const token = authHeader && authHeader.split(" ")[1];
+
+    jsonwebtoken.verify(token, secret, (err, user) => {
+
+        if (err) {
+            res.json({
+                error: "auth failed",
+                success: 0
+            })
+        } else {
+            connection.query("delete from likes where user_id = ? and post_id = ?", [user.id, id], (err, result) => {
+                if (err) {
+                    res.json({
+                        success: 0,
+                        error: err.message
+                    })
+                } else {
+                    res.json({
+                        success: 1,
+                    })
+                }
+            })
+        }
+    });
+
+
+
+})
+
+postRouter.get("/checklike/:id", authMiddleware, (req, res) => {
+
+    const id = req.params.id;
+
+    const authHeader = req.headers['authorization'];
+
+    console.log("hello1", authHeader)
+
+    const token = authHeader && authHeader.split(" ")[1];
+
+    jsonwebtoken.verify(token, secret, (err, user) => {
+
+        if (err) {
+            res.json({
+                error: "auth failed",
+                success: 0
+            })
+        } else {
+            connection.query("select * from likes where user_id = ? and post_id = ?", [user.id, id], (err, result) => {
+                if (err) {
+                    res.json({
+                        success: 0,
+                        error: err.message
+                    })
+                } else {
+                    if (result.length > 0) {
+                        res.json({
+                            success: 1,
+                        })
+                    } else {
+                        res.json({
+                            success: 0,
+                        })
+                    }
+                }
+            })
+        }
+    });
+
+})
+
+
+postRouter.get('/countlikes/:id', authMiddleware, (req, res) => {
+
+    const id = req.params.id;
+
+    connection.query("select count(*) as count from likes where post_id = ?", [id], (err, result) => {
+        if (err) {
+            res.json({
+                success: 0,
+                error: err.message
+            })
+        } else {
+            if (result.length > 0) {
+                res.json({
+                    result: result[0]
+                })
+            } else {
+                res.json({
+                    success: 0,
                 })
             }
         }
